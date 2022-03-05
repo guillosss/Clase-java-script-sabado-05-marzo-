b4b60ae1531fc340f1f2ef3c9c8c651b86d57f80
209 <209@academicos.cesde.edu.co>
2022-03-05 17:37:08 -0500

    Se hace arreglo

--- usuarios.js
+++ usuarios.js
@@ -1,18 +1,18 @@
 let usuarios=[
-    {nombre:"juan",edad:"3",esPaisa:true},
-    {nombre:"jose",edad:"15",esPaisa:false},
-    {nombre:"pedro",edad:"20",esPaisa:true},
-    {nombre:"andres",edad:"25",esPaisa:false},
-    {nombre:"carlos",edad:"31",esPaisa:false},
-    {nombre:"eduardo",edad:"32",esPaisa:true},
-    {nombre:"sergios",edad:"33",esPaisa:true},
-    {nombre:"genny",edad:"65",esPaisa:true},
-    {nombre:"gloria",edad:"40",esPaisa:true},
-    {nombre:"restrepo",edad:"14",esPaisa:true},
+    {nombre:"juan",edad:3,esPaisa:true},
+    {nombre:"jose",edad:15,esPaisa:false},
+    {nombre:"pedro",edad:20,esPaisa:true},
+    {nombre:"andres",edad:25,esPaisa:false},
+    {nombre:"carlos",edad:51,esPaisa:false},
+    {nombre:"eduardo",edad:32,esPaisa:true},
+    {nombre:"sergios",edad:33,esPaisa:true},
+    {nombre:"genny",edad:65,esPaisa:true},
+    {nombre:"gloria",edad:40,esPaisa:true},
+    {nombre:"restrepo",edad:14,esPaisa:true},
 ]
 //filtrar el arreglo para obtener los usuarios mayores de edad
 
-let filtrado=usuarios.filter(function(usuario){
+/*let filtrado=usuarios.filter(function(usuario){
     return(usuario.edad>=18)
 
 })
@@ -29,9 +29,15 @@ console.log(usuarios.filter(function(usuario){
 
 
 console.log(usuarios.map(function(usuario){
-    return(usuario.edad=100)
+return(usuario.edad=100)
 }))
 
 console.log(usuarios.map(function(usuario){
     return(usuario.nombre+" cesde")
-}))
+}))*/
+
+let suma=0
+
+usuarios.forEach(function(usuario) {
+    console.log(suma=suma+usuario.edad)
+});
